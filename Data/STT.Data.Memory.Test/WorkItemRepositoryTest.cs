2f114a3936683a958eeedabf3fbebce643cd5322
Ivan Fabijanovic <ivan-fabijanovic@live.com>
2013-08-04 12:45:45 +0200

    Simplified the model by removing the parent-child relationship from WorkItem.
    Signed-off-by: Ivan Fabijanovic <ivan-fabijanovic@live.com>

--- Data/STT.Data.Memory.Test/WorkItemRepositoryTest.cs
+++ Data/STT.Data.Memory.Test/WorkItemRepositoryTest.cs
@@ -79,30 +79,6 @@ namespace STT.Data.Memory.Test
         }
 
         [Test]
-        public void FindItemWithChildren()
-        {
-            var repository = GetRepository();
-
-            var workItem = new WorkItem();
-
-            var child1 = new WorkItem{Parent = workItem};
-            var child2 = new WorkItem{Parent = workItem};
-            var child3 = new WorkItem {Parent = workItem};
-
-            workItem.Children.Add(child1);
-            workItem.Children.Add(child2);
-            workItem.Children.Add(child3);
-
-            repository.Save(workItem);
-
-            var savedItem = repository.Find(workItem.Key);
-            var children = savedItem.Children.ToList();
-            Assert.That(children, Contains.Item(child1));
-            Assert.That(children, Contains.Item(child2));
-            Assert.That(children, Contains.Item(child3));
-        }
-
-        [Test]
         public void GetItems()
         {
             var repository = GetRepository();
@@ -123,43 +99,6 @@ namespace STT.Data.Memory.Test
             foreach (var item in items)
             {
                 Assert.That(savedItems, Contains.Item(item));
-            }
-        }
-
-        [Test]
-        public void GetItemsWithChildren()
-        {
-            var repository = GetRepository();
-
-            var workItem1 = new WorkItem();
-            var workItem2 = new WorkItem();
-            var items = new List<WorkItem> {workItem1, workItem2};
-
-            var child11 = new WorkItem {Parent = workItem1};
-            var child12 = new WorkItem {Parent = workItem1};
-            workItem1.Children.Add(child11);
-            workItem1.Children.Add(child12);
-
-            var child21 = new WorkItem { Parent = workItem2 };
-            var child22 = new WorkItem { Parent = workItem2 };
-            workItem2.Children.Add(child21);
-            workItem2.Children.Add(child22);
-
-            repository.Save(workItem1);
-            repository.Save(workItem2);
-
-            var savedItems = repository.GetWithChildren();
-            foreach (var savedItem in savedItems)
-            {
-                var item = items.SingleOrDefault(x => x.Key == savedItem.Key);
-                if (item != null)
-                {
-                    var children = savedItem.Children.ToList();
-                    foreach (var child in item.Children)
-                    {
-                        Assert.That(children, Contains.Item(child));
-                    }
-                }
             }
         }
     }
--- Data/STT.Data/IWorkItemRepository.cs
+++ Data/STT.Data/IWorkItemRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using STT.Model.Entity;
+﻿using STT.Model.Entity;
 
 namespace STT.Data
 {
     public interface IWorkItemRepository : IRepositoryBase<WorkItem>
     {
-        IEnumerable<WorkItem> GetWithChildren();
-        WorkItem FindWithChildren(Guid key);
     }
 }
